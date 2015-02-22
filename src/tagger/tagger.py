89eba4de417053a74cf90cb0a346d82e5559c50d
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2015-02-22 11:19:42 +0530

    Stem and sanitize each word

--- src/tagger/tagger.py
+++ src/tagger/tagger.py
@@ -2,13 +2,30 @@
 __author__ = 'Somsubhra Bairi'
 __email__ = 'somsubhra.bairi@gmail.com'
 
+# All imports
+from nltk import PorterStemmer
+
 
 # Tags the text
 class Tagger:
 
+    # Constructor for the Tagger
     def __init__(self):
         pass
 
+    # Tag the text
     @staticmethod
     def tag(text):
-        return text
+        result = ""
+        words = text.split()
+        for word in words:
+
+            # Strip the special characters
+            sanitized_word = ''.join(e for e in word if e.isalnum())
+
+            # Stem the word
+            stemmed_word = PorterStemmer().stem_word(sanitized_word)
+
+            result += stemmed_word + " "
+
+        return result
